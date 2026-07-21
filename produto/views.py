1a89c7b96239b21fabb1cb8a1aa04a2083fc4d19
Gusbzz <gusbzz56@gmail.com>
2026-07-21 17:49:37 -0300

    Métodos GET e POST adicionados

--- produto/views.py
+++ produto/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def ver_produto(request):
-    return render(request, 'ver_produto.html' , {'nome': 'Gustavo'})
+    if request.method == "GET":
+        nome = 'Caio'
+        return render(request, 'ver_produto.html' , {'nome': 'nome'})
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        idade = request.POST.get('idade')
 
 def inserir_produto(request):
     return HttpResponse('Estou no inserir')
